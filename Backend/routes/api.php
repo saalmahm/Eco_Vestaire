96487d58f8a84ee5f3e4267e4ab395ebd0b20b6b
salma hamdi <saalmahm51@gmail.com>
2025-04-01 16:30:38 +0000

    Ajout des routes necessaires pour Categories

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,20 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+// AUT H E N T I C A T I O N
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-
-// Route::middleware('auth:sanctum')->group(function () {
-// });
-
+// I T E M S
 Route::get('/items/trending', [ItemController::class, 'trending']);
 Route::get('/items/search', [ItemController::class, 'search']);
 Route::get('/items/seller', [ItemController::class, 'bySeller']);
-Route::get('/items/category/{categoryId}', [ItemController::class, 'byCategory']);
+Route::get('/items/category/{categoryId}', [ItemController::class, 'byCategory']);  
 Route::apiResource('items', ItemController::class);
+// C A T E G O R Y
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class)->except(['index', 'show']);
+});
+Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
