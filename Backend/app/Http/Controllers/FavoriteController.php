be314087826fb0eb9922c8351b265cfd2aa3bca8
salma hamdi <saalmahm51@gmail.com>
2025-04-01 23:17:40 +0000

    Creation du CRUD necessaire pour FavotiteController AVEC les tests sur Postman

--- Backend/app/Http/Controllers/FavoriteController.php
+++ Backend/app/Http/Controllers/FavoriteController.php
@@ -3,63 +3,78 @@
 namespace App\Http\Controllers;
 
 use App\Models\Favorite;
+use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Get all user's liked items
      */
     public function index()
     {
-        //
+        $favorites = Auth::user()
+            ->favorites()
+            ->with(['item.seller', 'item.category'])
+            ->paginate(10);
+
+        return response()->json(['data' => $favorites]);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Like an item
      */
-    public function create()
+    public function like(Item $item)
     {
-        //
+        if (Auth::user()->hasLiked($item)) {
+            return response()->json(['message' => 'Item already liked'], 422);
+        }
+
+        $favorite = Favorite::create([
+            'user_id' => Auth::id(),
+            'item_id' => $item->id
+        ]);
+
+        return response()->json([
+            'data' => $favorite->load('item')
+        ], 201);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Unlike an item
      */
-    public function store(Request $request)
+    public function unlike(Item $item)
     {
-        //
+        $deleted = Auth::user()
+            ->favorites()
+            ->where('item_id', $item->id)
+            ->delete();
+
+        if (!$deleted) {
+            return response()->json(['message' => 'Like not found'], 404);
+        }
+
+        return response()->json(['message' => 'Removed Item from favorites'], 200);
     }
 
     /**
-     * Display the specified resource.
+     * Check if user liked an item
      */
-    public function show(Favorite $favorite)
+    public function checkLike(Item $item)
     {
-        //
+        return response()->json([
+            'liked' => Auth::user()->hasLiked($item)
+        ]);
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Get the number of likes for an item
      */
-    public function edit(Favorite $favorite)
+    public function likesCount(Item $item)
     {
-        //
-    }
+        $likesCount = $item->likesCount();
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Favorite $favorite)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Favorite $favorite)
-    {
-        //
+        return response()->json(['likes_count' => $likesCount]);
     }
 }
