27c98a7fa754953e6bf93ff85bbc4ebf16343594
salma hamdi <saalmahm51@gmail.com>
2025-04-01 15:13:59 +0000

    Ajout des routes necessaires pour Items

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,12 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Route::middleware('auth:sanctum')->group(function () {
+// });
+
+Route::get('/items/trending', [ItemController::class, 'trending']);
+Route::get('/items/search', [ItemController::class, 'search']);
+Route::get('/items/seller', [ItemController::class, 'bySeller']);
+Route::get('/items/category/{categoryId}', [ItemController::class, 'byCategory']);
+Route::apiResource('items', ItemController::class);
